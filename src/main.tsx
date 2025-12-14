70569de752f134d4648e4412762cbb51c9f2ea7e
iudd <86305479+iudd@users.noreply.github.com>
2025-12-14 19:03:42 +0800

    Update to full OmniModel Hub chat interface

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-console.log('Main.tsx loaded');
-
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.createRoot(root).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
-} else {
-  console.error('Root element not found');
-}
+${input}
